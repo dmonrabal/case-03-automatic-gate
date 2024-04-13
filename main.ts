88cf5f3db899880ed21307dbec6381dfee4297f6
dmonrabal <36498138+dmonrabal@users.noreply.github.com>
2024-04-13 17:33:22 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+let i = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # . # .
+    # . . . #
+    . # # # .
+    `)
+neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 180)
+basic.forever(function () {
+    i = PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)
+    if (i > 3 && i < 20) {
+        neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 240)
+        basic.pause(5000)
+    } else {
+        neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 180)
+    }
+})
